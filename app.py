92cf784946c3d9b246109c5b2b5b7567a3378f3e
Valera566-alt <valera.personal8a@gmail.com>
2026-09-15 19:41:06 +0300

    Обновление дизайна: адаптивная верстка, иконки услуг и блок FAQ

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from services import get_site_info
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World! Flask работает в Kali."
+@app.route("/")
+def index():
+    # Просто передаем весь словарь в шаблон. Никаких f-string здесь быть не должно.
+    return render_template("index.html", **get_site_info())
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(debug=True)
