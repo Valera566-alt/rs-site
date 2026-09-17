c1394eb6071f6e4f8262a9dca8d02f57818651a1
Valera566-alt <valera.personal8a@gmail.com>
2026-09-17 20:14:21 +0300

    Версия 17 кнопка и меню с разделителем. адаптация под медиа запрос

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from services import get_site_info
 
 app = Flask(__name__)
 
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route("/")
 def index():
     # Просто передаем весь словарь в шаблон. Никаких f-string здесь быть не должно.
